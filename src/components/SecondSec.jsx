dabdc49fc8a031dbb7a72fd3e8e4734ccbd4da89
shxblx <shiblibasheer27@gmail.com>
2024-11-24 13:52:49 +0530

    upcoming aligned

--- src/components/SecondSec.jsx
+++ src/components/SecondSec.jsx
@@ -7,7 +7,7 @@ const SecondSec = () => {
       <div className="flex justify-center mt-8 text-[#3d52a0] text-2xl font-bold">
         <p>Upcoming Weather</p>
       </div>
-      <div className="flex space-x-3 md:space-x-10 justify-center mt-[30px]">
+      <div className="flex space-x-3 ml-2 mr-2 md:space-x-10 justify-center mt-[30px]">
         <div className="bg-[#3d52a0] h-[150px] w-[200px] md:h-[200px] md:w-[150px] rounded-2xl"></div>
         <div className="bg-[#3d52a0] h-[150px] w-[200px] md:h-[200px] md:w-[150px] rounded-2xl"></div>
         <div className="bg-[#3d52a0] h-[150px] w-[200px] md:h-[200px] md:w-[150px] rounded-2xl"></div>
